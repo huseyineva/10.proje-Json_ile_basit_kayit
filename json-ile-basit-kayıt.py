6823a92b3586d6f266b0f7cc01cda6452a8c7f20
Hüseyin Diyaroğlu <98124954+huseyineva@users.noreply.github.com>
2022-04-28 15:20:04 +0300

    Add files via upload

---
+++ json-ile-basit-kayıt.py
@@ -0,0 +1,90 @@
+import json
+import os
+
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+class UserRepository:
+    def __init__(self):
+        self.users = []
+        self.isLoggedin = False
+        self.currentUser = {}
+
+        self.loadUsers()
+    
+    def loadUsers(self):
+        if os.path.exists('users.json'):
+            with open('users.json', 'r', encoding='utf-8') as file:
+                users = json.load(file)
+                for user in users:
+                    user = json.loads(user)
+                    newUser = User(username = user['username'], password = user['password'], email = user['email']) 
+                    self.users.append(newUser)
+            print(self.users)
+
+    def register(self, user: User):
+        self.users.append(user)
+        self.savetoFile
+        print("Kullanıcı oluşturuldu!")
+   
+    def login(self, username, password):
+        for user in self.users:
+            if user.username == username and user.password == password:
+                self.isLoggedin = True
+                self.currentUser = user
+                print("Giriş yapıldı!")
+                break
+        
+    def savetoFile(self):
+
+        list = []
+
+        for user in self.users:
+            list.append(json.dumps(user.__dict__))
+
+        with open('users.json', 'w') as file:
+            json.dump(list, file)
+
+    def logout(self):
+        self.isLoggedin = False
+        self.currentUser = {}
+        print("Çıkış yapıldı!")
+
+    def identity(self):
+        if self.isLoggedin:
+            print(f'username: {self.currentUser.username}')
+        else:
+            print("Giriş yapılmadı!")
+
+repository = UserRepository()
+
+while True:
+    print('Menü'.center(50,'*'))
+    secim = input('1- Register\n2- Login\n3- Logout\n4- Identity\n5- Exit\nSeçiminiz: ')
+    
+    if secim == '5':
+        break
+    else:
+        if secim == '1':
+            username = input("username: ")
+            password = input("password: ")
+            email = input("email: ")
+            user = User(username=username, password=password, email=email)
+            repository.register(user)
+        elif secim == '2':
+            if repository.isLoggedin:
+                print("Önceden giriş yaptınız...")
+            else:
+                username = input("Username: ")
+                password = input("password: ")
+                repository.login(username, password)    
+        elif secim == '3':
+            repository.logout()
+        elif secim == '4':
+            repository.identity()
+        else:
+            print("Yanlış seçim yaptınız!")
+    
